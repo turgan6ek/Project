0efb536270c4ffd3be3cb86ab5dc3d036caa81ee
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-09-30 00:13:33 +0600

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ImgCarouselComponent } from './img-carousel/img-carousel.component';
-
+import {CarouselModule, MDBBootstrapModule, WavesModule} from 'angular-bootstrap-md';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +17,10 @@ import { ImgCarouselComponent } from './img-carousel/img-carousel.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    CarouselModule,
+    MDBBootstrapModule.forRoot(),
+    WavesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
