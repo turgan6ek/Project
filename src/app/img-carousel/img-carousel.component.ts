d7da13d8ffaf375e6f72a38a11e514c5f4fe8395
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-09-29 20:36:25 +0600

    1

---
+++ src/app/img-carousel/img-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img-carousel',
+  templateUrl: './img-carousel.component.html',
+  styleUrls: ['./img-carousel.component.css']
+})
+export class ImgCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
