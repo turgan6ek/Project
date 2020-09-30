26f8ca56fa13f5125ae221cbdc7e9a62fb0cea37
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-09-30 22:05:07 +0600

    1

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, Input, SimpleChanges, OnChanges, DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.css']
 })
-export class CardListComponent implements OnInit {
+export class CardListComponent implements OnInit , DoCheck, OnChanges{
   @Input() likes = 0;
+  oldLikes=0;
   constructor() { }
   @Output() newLikeEvent = new EventEmitter<string>();
 
@@ -17,4 +18,39 @@ export class CardListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+  ngDoCheck() {
+
+    if (this.likes!== this.oldLikes) {
+      this.changeDetected = true;
+      this.changeLog.push(`DoCheck: Hero name changed to "${this.hero.name}" from "${this.oldHeroName}"`);
+      this.oldHeroName = this.hero.name;
+    }
+
+    if (this.power !== this.oldPower) {
+      this.changeDetected = true;
+      this.changeLog.push(`DoCheck: Power changed to "${this.power}" from "${this.oldPower}"`);
+      this.oldPower = this.power;
+    }
+
+    if (this.changeDetected) {
+      this.noChangeCount = 0;
+    } else {
+      // log that hook was called when there was no relevant change.
+      const count = this.noChangeCount += 1;
+      const noChangeMsg = `DoCheck called ${count}x when no change to hero or power`;
+      if (count === 1) {
+        // add new "no change" message
+        this.changeLog.push(noChangeMsg);
+      } else {
+        // update last "no change" message
+        this.changeLog[this.changeLog.length - 1] = noChangeMsg;
+      }
+    }
+
+    this.changeDetected = false;
+  }
 }
--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -1,18 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styleUrls: ['./list-page.component.css']
 })
-export class ListPageComponent implements OnInit {
+export class ListPageComponent implements OnInit, OnChanges {
 
   likes = 0;
   constructor() {
     console.log("ChildComponent:Constructor");
   }
   ngOnInit(): void {
-  this.likes = 0;
-
+    this.likes = 0;
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+  }
+
+
+
 }
