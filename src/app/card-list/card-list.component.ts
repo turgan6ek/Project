1e888e11372474479d86ab64499595ff5b794db4
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-09-30 22:19:59 +0600

    1

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -8,12 +8,19 @@ import {Component, OnInit, Output, EventEmitter, Input, SimpleChanges, OnChanges
 export class CardListComponent implements OnInit , DoCheck, OnChanges{
   @Input() likes = 0;
   oldLikes=0;
+  text;
   constructor() { }
   @Output() newLikeEvent = new EventEmitter<string>();
 
   addLike() {
     this.likes++;
     this.likes = this.likes % 2;
+    if (this.likes == 1){
+      this.text = "You liked this text";
+    }
+    else {
+      this.text = '';
+    }
   }
   ngOnInit(): void {
   }
@@ -22,35 +29,5 @@ export class CardListComponent implements OnInit , DoCheck, OnChanges{
   ngOnChanges(changes: SimpleChanges): void {
 
   }
-  ngDoCheck() {
-
-    if (this.likes!== this.oldLikes) {
-      this.changeDetected = true;
-      this.changeLog.push(`DoCheck: Hero name changed to "${this.hero.name}" from "${this.oldHeroName}"`);
-      this.oldHeroName = this.hero.name;
-    }
-
-    if (this.power !== this.oldPower) {
-      this.changeDetected = true;
-      this.changeLog.push(`DoCheck: Power changed to "${this.power}" from "${this.oldPower}"`);
-      this.oldPower = this.power;
-    }
-
-    if (this.changeDetected) {
-      this.noChangeCount = 0;
-    } else {
-      // log that hook was called when there was no relevant change.
-      const count = this.noChangeCount += 1;
-      const noChangeMsg = `DoCheck called ${count}x when no change to hero or power`;
-      if (count === 1) {
-        // add new "no change" message
-        this.changeLog.push(noChangeMsg);
-      } else {
-        // update last "no change" message
-        this.changeLog[this.changeLog.length - 1] = noChangeMsg;
-      }
-    }
-
-    this.changeDetected = false;
-  }
+  ngDoCheck() {}
 }
